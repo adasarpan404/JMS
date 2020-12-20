40e3c4a3a6e2eb12675ea33de7bb4c0436c053b7
arpan  das <dasarpan471@gmail.com>
2020-12-20 19:44:40 +0530

    node"

--- Controllers/viewController.js
+++ Controllers/viewController.js
@@ -2,6 +2,8 @@ const catchAsync = require('../Utils/catchAsync');
 
 
 
+
 exports.getOverview = catchAsync(async (req, res, next) => {
-    res.status(200).render('base');
+    const tours = await
+        res.status(200).render('base');
 })
