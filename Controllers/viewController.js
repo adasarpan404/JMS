2a59639f93f4b0ed0130811813b0946b3b018074
arpan  das <dasarpan471@gmail.com>
2020-12-22 14:36:45 +0530

    server side rendering started

--- Controllers/viewController.js
+++ Controllers/viewController.js
@@ -4,6 +4,16 @@ const catchAsync = require('../Utils/catchAsync');
 
 
 exports.getOverview = catchAsync(async (req, res, next) => {
-    const tours = await
-        res.status(200).render('base');
+    res.status(200).render('startingPage');
+})
+exports.getLoginPage = catchAsync(async (req, res, next) => {
+    res.status(200).render('base', {
+        purpose: 'this is for login page'
+    })
+})
+
+exports.getMaidLoginPage = catchAsync(async (req, res, next) => {
+    res.status(200).render('base', {
+        purpose: 'This is for Maid login page'
+    })
 })
--- Router/viewRouter.js
+++ Router/viewRouter.js
@@ -2,4 +2,6 @@ const express = require('express')
 const viewController = require('../Controllers/viewController')
 const router = express.Router();
 router.get('/', viewController.getOverview)
+router.get('/login', viewController.getLoginPage);
+router.get('/maid-login', viewController.getMaidLoginPage)
 module.exports = router;
