506115066acfcf96e0b0f507ee47d202c5146a82
arpan  das <dasarpan471@gmail.com>
2021-01-24 15:44:44 +0530

    some error

--- Router/bookingRoute.js
+++ Router/bookingRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bookingController = require('../controllers/bookingController');
+const bookingController = require('./../controllers/bookingController');
 const authController = require('./../controllers/authController')
 const router = express.Router();
 
